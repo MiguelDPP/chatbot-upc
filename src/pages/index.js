13858a34187124e8a4e47e3a36bff76b4dafa275
miguecode <mipor.278@gmail.com>
2023-05-22 15:58:40 -0500

    Ajustado padding del chat

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ export default function Home() {
       <div className="sm:ml-64 mx-auto shadow-lg rounded-lg" onClick={() => setIsAsideActive(false)}>
         <div className={`flex flex-row justify-between bg-white ${styles.chat}`}>
           <div className="w-full px-5 flex flex-col justify-between">
-            <div className={`flex flex-col mt-5 overflow-y-auto p-4 ${styles.chatContent}`} ref={chat}>
+            <div className={`flex flex-col mt-5 overflow-y-auto ${styles.chatContent}`} ref={chat}>
               {/* Mensajes */}
               {messages.map((message, index) => {
                 return (
